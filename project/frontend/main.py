b7dde9deba60d3e6996df7b91e7326e1eb200d7e
Shuchi Agarwal <shuchia@fipointer.com>
2021-05-03 11:23:59 -0400

    Changes to make a form variable

--- project/frontend/main.py
+++ project/frontend/main.py
@@ -67,7 +67,7 @@ if st.button("Summarize"):
 
         taskId = task.get("uid")
 
-        time.sleep(5)
+        time.sleep(2)
 
         res = requests.get(f"http://web:8000/summaries/work/status?uid=" + str(taskId))
 
@@ -80,13 +80,14 @@ if st.button("Summarize"):
                 if url not in displayed_urls:
                     res = requests.get(f"http://web:8000/summaries/{summaryId}")
                     summaryResponse = res.json()
+                    st.write(url)
                     st.write(summaryResponse.get("summary"))
                     displayed_urls.append(url)
                     displayed += 1
                     my_bar.progress(displayed)
                     latest_iteration.text("Processed : " + str(displayed) + " summaries")
 
-            time.sleep(10)
+            time.sleep(2)
 
             res = requests.get(f"http://web:8000/summaries/work/status?uid=" + str(taskId))
             taskResponse = res.json()
