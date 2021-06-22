5c03ab52060c2034a1e80c3cd29ae82251218074
Shuchi Agarwal <shuchia@fipointer.com>
2021-06-21 20:18:18 -0400

    Changes for auth

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,20 +1,15 @@
 # project/app/api/summaries.py
 
-
+import httpx
 from fastapi import APIRouter, HTTPException, Path, BackgroundTasks, status
-from fastapi import File, UploadFile, Depends, Form
+from fastapi import File, UploadFile, Depends, Form, Header
 from fastapi.responses import HTMLResponse
-from typing import List, Dict
-import base64
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from typing import List, Dict, Optional
+
 from starlette.responses import RedirectResponse, Response, JSONResponse
 
-import jwt
-from jwt import PyJWTError
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2
-from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.openapi.docs import get_swagger_ui_html
 
 from fastapi.openapi.utils import get_openapi
 
@@ -36,26 +31,12 @@ from app.models.pydantic import (
 from app.models.tortoise import SummarySchema, ReportSchema
 from app.summarizer import generate_summary, generate_bulk_summary, generate_report
 from app.oauth2 import BasicAuth, OAuth2PasswordBearerCookie
-from starlette.status import HTTP_403_FORBIDDEN
-from fastapi.encoders import jsonable_encoder
 
 SECRET_KEY = "a9032cb3b87e7ad1d842e1a20fbf22901a2826d359a63ab6a6b6a8a7d1e9c019"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 180
-fake_users_db = {
-    "nsbharath": {
-        "username": "nsbharath",
-        "full_name": "NS Bharath",
-        "email": "nsbharath@yahoo.com",
-        "hashed_password": "Zmlwb2ludGVyMTIz",
-        "disabled": False,
-    }
-}
-basic_auth = BasicAuth(auto_error=False)
 
 security = HTTPBasic()
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearerCookie(tokenUrl="/token")
 
@@ -63,62 +44,19 @@ router = APIRouter()
 jobs: Dict[UUID, Job] = {}
 
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(*, data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except PyJWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+def has_access(credentials: HTTPBasicCredentials = Depends(security), authorization: Optional[str] = Header(None)):
+    if not authorization:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='No access to resource. Credentials missing!',
+        )
+    headers = {'Authorization': authorization}
+    result = httpx.get("http://ec2-54-152-94-32.compute-1.amazonaws.com:8002/api/access/auth", headers=headers)
+    if result.status_code == 401:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='No access to resource. Login first.',
+        )
 
 
 @router.get("/")
@@ -126,81 +64,17 @@ async def homepage():
     return "Welcome to the security test!"
 
 
-@router.post("/token", response_model=Token)
-async def route_login_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@router.get("/logout")
-async def route_logout_and_remove_cookie():
-    response = RedirectResponse(url="/")
-    response.delete_cookie("Authorization", domain="ec2-54-152-94-32.compute-1.amazonaws.com")
-    return response
-
-
-@router.get("/login_basic")
-async def login_basic(credentials: HTTPBasicCredentials = Depends(security)):
-    username = credentials.username
-    password = credentials.password
-    if not (username and password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Incorrect email or password.',
-            headers={'WWW-Authenticate': 'Basic'},
-        )
-
-    try:
-
-        user = authenticate_user(fake_users_db, username, password)
-        if not user:
-            raise HTTPException(status_code=400, detail="Incorrect email or password")
-
-        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-        access_token = create_access_token(
-            data={"sub": username}, expires_delta=access_token_expires
-        )
-
-        token = jsonable_encoder(access_token)
-
-        response = RedirectResponse(url="/docs")
-        response.set_cookie(
-            "Authorization",
-            value=f"Bearer {token}",
-            domain="ec2-54-152-94-32.compute-1.amazonaws.com",
-            httponly=True,
-            max_age=1800,
-            expires=1800,
-        )
-        return response
-
-    except:
-        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
-        return response
-
-
-@router.get("/openapi.json")
-async def get_open_api_endpoint(current_user: User = Depends(get_current_active_user)):
+@router.get("/openapi.json", dependencies=[Depends(has_access)])
+async def get_open_api_endpoint():
     return JSONResponse(get_openapi(title="FastAPI", version=1, routes=router.routes))
 
 
-@router.get("/docs")
-async def get_documentation(current_user: User = Depends(get_current_active_user)):
+@router.get("/docs", dependencies=[Depends(has_access)])
+async def get_documentation():
     return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
 
 
-@router.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
-
-@router.post("/bulk", response_model=Job, status_code=202)
+@router.post("/bulk", response_model=Job, status_code=202, dependencies=[Depends(has_access)])
 async def create_summary(
         background_tasks: BackgroundTasks, modelname: str = Form(...), file: UploadFile = File(...)
 ) -> SummaryResponseSchema:
--- project/app/main.py
+++ project/app/main.py
@@ -2,22 +2,109 @@
 
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from passlib.context import CryptContext
 
 from app.api import ping, summaries
 from app.db import init_db
+from app.models.pydantic import (
+UserInDB,
+User)
 
 log = logging.getLogger(__name__)
 
+fake_users_db = {
+    "nsbharath": {
+        "username": "nsbharath",
+        "full_name": "NS Bharath",
+        "email": "nsbharath@yahoo.com",
+        "hashed_password": "Zmlwb2ludGVyMTIz",
+        "disabled": False,
+    }
+}
+security = HTTPBasic()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def create_application() -> FastAPI:
-    application = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+    application = FastAPI()
     application.include_router(ping.router)
     application.include_router(
         summaries.router, prefix="/summaries", tags=["summaries"]
     )
 
     return application
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+# def create_access_token(*, data: dict, expires_delta: timedelta = None):
+#     to_encode = data.copy()
+#     if expires_delta:
+#         expire = datetime.utcnow() + expires_delta
+#     else:
+#         expire = datetime.utcnow() + timedelta(minutes=15)
+#     to_encode.update({"exp": expire})
+#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+#     return encoded_jwt
+#
+#
+# async def get_current_user(token: str = Depends(oauth2_scheme)):
+#     credentials_exception = HTTPException(
+#         status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+#     )
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         username: str = payload.get("sub")
+#         if username is None:
+#             raise credentials_exception
+#         token_data = TokenData(username=username)
+#     except PyJWTError:
+#         raise credentials_exception
+#     user = get_user(fake_users_db, username=token_data.username)
+#     if user is None:
+#         raise credentials_exception
+#     return user
+#
+#
+# async def get_current_active_user(current_user: User = Depends(get_current_user)):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
+
+
+def authorize(credentials: HTTPBasicCredentials = Depends(security)):
+    user = authenticate_user(fake_users_db, credentials.username, credentials.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Incorrect email or password.',
+            headers={'WWW-Authenticate': 'Basic'},
+        )
 
 
 app = create_application()
@@ -32,3 +119,8 @@ async def startup_event():
 @app.on_event("shutdown")
 async def shutdown_event():
     log.info("Shutting down...")
+
+
+@app.get('/api/access/auth', dependencies=[Depends(authorize)])
+def auth():
+    return {"Granted": True}
