6b911589913130853fe63167b0cb93639047fcc4
Shuchi Agarwal <shuchia@fipointer.com>
2021-07-16 09:13:29 -0400

    Changes for length

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -129,7 +129,7 @@ async def create_summary(
 
     background_tasks.add_task(generate_summary, summary_id, payload.url, payload.model_name, payload.length)
 
-    response_object = {"id": summary_id, "url": payload.url, "model_name": payload.model_name}
+    response_object = {"id": summary_id, "url": payload.url, "model_name": payload.model_name, "length": payload.length}
     return response_object
 
 
