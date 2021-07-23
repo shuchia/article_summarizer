93b3f8784b9840391ffa34e154a6d0254eda31ee
Shuchi Agarwal <shuchia@fipointer.com>
2021-07-23 19:12:18 -0400

    Changes for docker file

--- project/app/summarypro.py
+++ project/app/summarypro.py
@@ -12,7 +12,7 @@ import logging
 
 nltk.download('punkt')
 log = logging.getLogger(__name__)
-torch_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 log.info(torch_device)
 PERCENTAGE = {"short": 10,
               "medium": 20,
