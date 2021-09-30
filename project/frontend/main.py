5fcb851217464f638228987a548061368bf1f4bc
Shuchi Agarwal <shuchia@fipointer.com>
2021-09-30 16:06:02 -0400

    Changes for page reconfig

--- project/frontend/main.py
+++ project/frontend/main.py
@@ -20,9 +20,10 @@ def main():
     )
     # Register your pages
     pages = {
-        "Summarize": page_first,
-        "Generate Reports": page_second,
-        "Retrieve Reports": page_third
+        "Summarizer ": page_first,
+        "Bulk Summaries ": page_second,
+        "Generate Reports": page_third,
+        "Retrieve Reports": page_fourth
     }
 
     st.sidebar.title("Summary Wizard")
@@ -50,125 +51,123 @@ def get_report_download_link(report_id, name):
 
 
 content_options = [
-    "CNN",
-    "Newsroom",
-    "CNN/DailyMail",
-    "Gigaword",
-    "Emails",
-    "Scholarly Articles",
-    "Patents",
-    "US Congressional bills",
-    "MultiNews",
-    "Medical publications",
-    "Reddit",
-    "WikiHow",
-    "Exterme Summary"
+    "FiPointer-Basic",
+    # "Newsroom",
+    # "CNN/DailyMail",
+    # "Gigaword",
+    # "Emails",
+    # "Scholarly Articles",
+    # "Patents",
+    # "US Congressional bills",
+    # "MultiNews",
+    # "Medical publications",
+    # "Reddit",
+    # "WikiHow",
+    # "Exterme Summary"
 
 ]
 
 TYPES = {
-    "CNN": "facebook/bart-large-cnn",
-    "Newsroom": "google/pegasus-newsroom",
-    "CNN/DailyMail": "feathers",
-    "Gigaword": "la_muse",
-    "Emails": "mosaic",
-    "Scholarly Articles": "starry_night",
-    "Patents": "starry_night",
-    "US Congressional bills": "starry_night",
-    "MultiNews": "starry_night",
-    "Medical publications": "starry_night",
-    "Reddit": "starry_night",
-    "WikiHow": "starry_night",
-    "Exterme Summary": "starry_night"
+    "FiPointer-Basic": "facebook/bart-large-cnn",
+    # "Newsroom": "google/pegasus-newsroom",
+    # "CNN/DailyMail": "feathers",
+    # "Gigaword": "la_muse",
+    # "Emails": "mosaic",
+    # "Scholarly Articles": "starry_night",
+    # "Patents": "starry_night",
+    # "US Congressional bills": "starry_night",
+    # "MultiNews": "starry_night",
+    # "Medical publications": "starry_night",
+    # "Reddit": "starry_night",
+    # "WikiHow": "starry_night",
+    # "Exterme Summary": "starry_night"
 
 }
 
 length_options = ['short', 'medium', 'long']
 
 
-def page_first():
+def page_second():
     st.set_option("deprecation.showfileUploaderEncoding", False)
-    st.title("Text Summarization")
-    col1, col2 = st.columns([5, 5])
-    with col1:
-        with st.form(key='form1'):
-            file = st.file_uploader("Upload an excel file", type="xlsx")
-            contentType = st.selectbox("Choose the type", options=content_options)
-            length = st.select_slider("Choose  length of the summary", options=length_options)
+    st.title("Generate summary reports")
+    with st.form(key='form1'):
+        file = st.file_uploader("Upload an excel file", type="xlsx")
+        contentType = st.selectbox("Choose the type", options=content_options)
+        length = st.select_slider("Choose  length of the summary", options=length_options)
 
-            username = st.text_input("Enter username")
-            password = st.text_input("Enter password", type="password")
+        username = st.text_input("Enter username")
+        password = st.text_input("Enter password", type="password")
 
-            submitted1 = st.form_submit_button('Generate Summaries')
-            session_state = SessionState.get(name="", submitted1=False)
+        submitted1 = st.form_submit_button('Generate Summaries')
+        session_state = SessionState.get(name="", submitted1=False)
 
-            if submitted1:
-                session_state.submitted1 = True
-            if session_state.submitted1:
-                usrPass = username + ":" + password
-                b64Val = base64.b64encode(usrPass.encode()).decode()
-                if file is not None and contentType is not None:
-                    files = {"file": (file.name, file.getvalue(), file.type)}
-                    # print(file.getvalue())
-                    df = pd.read_excel(file.read(), index_col=None, header=None)
-                    df1 = df.iloc[1:]
-                    total = len(df1)
-                    print(total)
-                    displayed = 0
-                    displayed_urls = []
-                    model = TYPES[contentType]
-                    headers = {'Content-type': 'multipart/form-data'}
-                    payload = {"model_name": model, "length": length}
-                    st.write("Generating summaries...")
-                    my_bar = st.progress(0)
-                    print(usrPass)
-                    print(b64Val)
-                    headers = {"Authorization": "Basic %s" % b64Val}
-                    res = requests.post(f"http://web:8000/summaries/bulk", data=payload, files=files,
-                                        headers=headers, verify=False)
+        if submitted1:
+            session_state.submitted1 = True
+        if session_state.submitted1:
+            usrPass = username + ":" + password
+            b64Val = base64.b64encode(usrPass.encode()).decode()
+            if file is not None and contentType is not None:
+                files = {"file": (file.name, file.getvalue(), file.type)}
+                # print(file.getvalue())
+                df = pd.read_excel(file.read(), index_col=None, header=None)
+                df1 = df.iloc[1:]
+                total = len(df1)
+                print(total)
+                displayed = 0
+                displayed_urls = []
+                model = TYPES[contentType]
+                headers = {'Content-type': 'multipart/form-data'}
+                payload = {"model_name": model, "length": length}
+                st.write("Generating summaries...")
+                my_bar = st.progress(0)
+                print(usrPass)
+                print(b64Val)
+                headers = {"Authorization": "Basic %s" % b64Val}
+                res = requests.post(f"http://web:8000/summaries/bulk", data=payload, files=files,
+                                    headers=headers, verify=False)
 
-                    task = res.json()
-                    print(task)
-                    latest_iteration = st.empty()
+                task = res.json()
+                print(task)
+                latest_iteration = st.empty()
 
-                    taskId = task.get("uid")
-                    st.write("Please use this UID to generate reports using the nav menu...")
-                    st.write(str(taskId))
+                taskId = task.get("uid")
+                st.write("Please use this UID to generate reports using the nav menu...")
+                st.write(str(taskId))
 
-                    # time.sleep(1)
-                    #
-                    # res = requests.get(f"http://web:8000/summaries/work/status?uid=" + str(taskId), headers=headers)
-                    #
-                    # taskResponse = res.json()
-                    # processed_urls = taskResponse.get("processed_ids")
-                    #
-                    # while taskResponse.get("status") == "in_progress":
-                    #     for summaryId in processed_urls.keys():
-                    #         url = processed_urls[summaryId]
-                    #         if url not in displayed_urls:
-                    #             res = requests.get(f"http://web:8000/summaries/{summaryId}",
-                    #                                headers=headers)
-                    #             summaryResponse = res.json()
-                    #             st.write(url)
-                    #             st.write(summaryResponse.get("summary"))
-                    #             displayed_urls.append(url)
-                    #             displayed += 1
-                    #             my_bar.progress(displayed)
-                    #             latest_iteration.text("Processed : " + str(displayed) + " summaries")
-                    #
-                    #     time.sleep(1)
-                    #
-                    #     res = requests.get(f"http://web:8000/summaries/work/status?uid=" + str(taskId),
-                    #                        headers=headers)
-                    #     taskResponse = res.json()
-                    #     processed_urls = taskResponse.get("processed_ids")
-                    # if taskResponse.get("status") == "Completed":
-                    #     res = requests.get(f"http://web:8000/summaries/generateReports?uid=" + str(taskId),
-                    #                        headers=headers)
-                    #     processed_reports = res.json()
-                    #     for reportId in processed_reports.keys():
-                    #         report_name = processed_reports[reportId]
-                    #         st.markdown(get_report_download_link(reportId, report_name), unsafe_allow_html=True)
+                # time.sleep(1)
+                #
+                # res = requests.get(f"http://web:8000/summaries/work/status?uid=" + str(taskId), headers=headers)
+                #
+                # taskResponse = res.json()
+                # processed_urls = taskResponse.get("processed_ids")
+                #
+                # while taskResponse.get("status") == "in_progress":
+                #     for summaryId in processed_urls.keys():
+                #         url = processed_urls[summaryId]
+                #         if url not in displayed_urls:
+                #             res = requests.get(f"http://web:8000/summaries/{summaryId}",
+                #                                headers=headers)
+                #             summaryResponse = res.json()
+                #             st.write(url)
+                #             st.write(summaryResponse.get("summary"))
+                #             displayed_urls.append(url)
+                #             displayed += 1
+                #             my_bar.progress(displayed)
+                #             latest_iteration.text("Processed : " + str(displayed) + " summaries")
+                #
+                #     time.sleep(1)
+                #
+                #     res = requests.get(f"http://web:8000/summaries/work/status?uid=" + str(taskId),
+                #                        headers=headers)
+                #     taskResponse = res.json()
+                #     processed_urls = taskResponse.get("processed_ids")
+                # if taskResponse.get("status") == "Completed":
+                #     res = requests.get(f"http://web:8000/summaries/generateReports?uid=" + str(taskId),
+                #                        headers=headers)
+                #     processed_reports = res.json()
+                #     for reportId in processed_reports.keys():
+                #         report_name = processed_reports[reportId]
+                #         st.markdown(get_report_download_link(reportId, report_name), unsafe_allow_html=True)
 
     with col2:
         with st.form(key='form2'):
@@ -206,8 +205,58 @@ def page_first():
                         break
 
 
-def page_second():
-    st.title("Generate Reports")
+def page_first():
+    st.title("Text Summarizer")
+    col1, col2 = st.columns([5, 5])
+    summaryResponse=''
+    with col1:
+        with st.form(key='summarize'):
+            placeholder = st.empty()
+            text_input = placeholder.text_area('text')
+            click_clear = st.button('Delete text', key=1)
+            if click_clear:
+                text_input = st.text_area('text', value="Paste text")
+            url_input = st.text_input(label='Enter a URL')
+            if url_input != '':
+                text_input.empty()
+            length = st.select_slider("Choose  length of the summary", options=length_options)
+            submitted2 = st.form_submit_button('Summarize')
+            if submitted2:
+                #     session_state.submitted2 = True
+                # if session_state.submitted2:
+                model = "facebook/bart-large-cnn"
+                if url_input != '':
+                    payload = {"url": url_input,
+                               "model_name": model,
+                               "length": length}
+                    st.write("Generating summary...")
+
+                    res = requests.post(f"http://web:8000/summaries/summary", json=payload)
+                    # time.sleep(10)
+                    summary_id = res.json().get("id")
+                    # print (summary_id)
+                    status = res.json().get("status")
+                    # print(status)
+                    taskId = res.json().get("task_id")
+                    while status == "in_progress":
+                        res = requests.get(f"http://web:8000/summaries/task/status?uid=" + str(taskId))
+                        taskResponse = res.json()
+                        if taskResponse.get("status") == "Completed":
+                            res = requests.get(f"http://web:8000/summaries/url_summary/{summary_id}/")
+                            summaryResponse = res.json()
+                            #st.write(summaryResponse.get("url"))
+                            # print(summaryResponse)
+                            #st.write(summaryResponse.get("summary"))
+                            break
+    with col2:
+        if summaryResponse != '':
+            st.write(summaryResponse.get("url"))
+            # print(summaryResponse)
+            st.write(summaryResponse.get("summary"))
+
+
+def page_third():
+    st.title("Generate reports")
     with st.form(key='generate'):
         taskId = st.text_input(label='Enter ID')
         username = st.text_input("Enter username")
@@ -225,7 +274,7 @@ def page_second():
                 st.markdown(get_report_download_link(reportId, report_name), unsafe_allow_html=True)
 
 
-def page_third():
+def page_fourth():
     st.title("Retrieve Reports")
     with st.form(key='retrieve'):
         topic = st.text_input(label='Enter Topic')
