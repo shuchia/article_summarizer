1515f33c6557ff490f70d8ce432902652dae6ad6
Shuchi Agarwal <shuchia@fipointer.com>
2021-10-14 10:47:26 -0400

    Changes for null url

--- project/app/summarizer.py
+++ project/app/summarizer.py
@@ -49,7 +49,7 @@ async def generate_summary(task: Job, summary_id: int, url: str, text: str, mode
         await Summary.filter(id=summary_id).update(summary=summary)
     except:
         log.exception("summary_id " + str(summary_id))
-    if url != "":
+    if url is not None:
         task.processed_ids[summary_id] = url
     elif text != "":
         task.processed_ids[summary_id] = text
