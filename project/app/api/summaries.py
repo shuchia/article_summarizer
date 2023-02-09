47b15c77efdaf741f772e86907b6f1bbc30d79e4
Shuchi Agarwal <shuchia@fipointer.com>
2023-02-08 21:59:00 -0500

    Changes for one report generation

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -118,7 +118,7 @@ async def create_summary(
     user = get_current_user(authorization)
     email = get_current_user_email(authorization)
     log.info("current user email " + email)
-    await log_requests(request)
+    # await log_requests(request)
     new_task = Job()
     jobs[new_task.uid] = new_task
     payload = BulkSummaryPayloadSchema(modelName=model_name)
