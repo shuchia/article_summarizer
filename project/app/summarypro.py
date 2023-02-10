e5f28fd52753a87784389b90fdd8fa9ef44b1369
Shuchi Agarwal <shuchia@fipointer.com>
2023-02-09 21:36:38 -0500

    Changes for one report generation

--- project/app/summarypro.py
+++ project/app/summarypro.py
@@ -53,7 +53,12 @@ def nest_sentences(document):
 
 
 def preprocess(url):
-    req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    req = Request(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,'
+                                          'image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                                'Accept-Encoding': 'gzip, deflate',
+                                'Accept-Language': 'en-US,en;q=0.9',
+                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                                              'like Gecko) Chrome/103.0.0.0 Safari/537.36'})
     scraped_data = urlopen(req, timeout=200)
 
     article = scraped_data.read()
